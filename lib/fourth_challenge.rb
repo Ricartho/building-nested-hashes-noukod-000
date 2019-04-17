5af76206355fe936007989dcceebe55c405390c7
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-17 12:53:59 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,7 +21,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-         {name: "Stevens",
+         {name: "Steven",
             age: "30",
             attitude: "confused"},
             
